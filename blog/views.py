85c76b06c380c5ec2c414fe8a1e3f836b80c2786
arya <raiabhishek04@gmail.com>
2016-03-05 21:02:45 +0530

    Checked Login Logout for edit purpose

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
 
 from blog.models import Post
 from blog.forms import PostForm
@@ -19,7 +21,7 @@ def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html',{'post': post})
 
-
+@login_required
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
@@ -33,7 +35,7 @@ def post_new(request):
 		form = PostForm()
 	return render(request, 'blog/post_edit.html', {'form': form})
 
-
+@login_required
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == "POST":
